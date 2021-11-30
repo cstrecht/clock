612a51dd3010cf468d58332815ed29f4721d4c12
Carolina Strecht <carolina.stsampaio@gmail.com>
2021-11-30 13:44:41 +0000

    digital js

---
+++ script.js
@@ -0,0 +1,21 @@
+let digital = document.querySelector(".digital");
+let hours = document.querySelector(".hours-indicator");
+let minutes = document.querySelector(".minutes-indicator");
+let seconds = document.querySelector(".seconds-indicator");
+
+//funcao que apresente o tempo a cada segundo que passe -> setInterval juntamente com a funcao a passar de 1 em 1 seg
+function showTime() {
+  let now = new Date();
+  let hour = now.getHours();
+  let min = now.getMinutes();
+  let sec = now.getSeconds();
+
+  // manipultar o html de forma a alterar os numeros à medida que os segundos passam:
+  digital.innerHTML = `${putZero(hour)}:${putZero(min)}:${putZero(sec)}`;
+}
+// funcao para colocar um zero nos mostradores com numeros menores que 10:
+function putZero(el) {
+  return el < 10 ? `0${el}` : el; // :D
+}
+// rodar a funcao de 1 em 1 segundo
+setInterval(showTime, 1000);
